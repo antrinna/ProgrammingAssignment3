cb129b9568ae1c187f29b51f2a07b5ca542f47ac
Anna Pestereva <antrinna@gmail.com>
2018-08-15 17:26:42 -0600

    complete rankhospital function, find the hospital in a state with the requested rank for a particular outcome

--- rankhospital.R
+++ rankhospital.R
@@ -16,18 +16,35 @@ rankhospital <- function(state, outcome, num = "best") {
   if(!(outcome %in% outcomes$n)){
     stop("invalid outcome")
   }
+
+  stateoutcomes <- outcomedata[outcomedata$State == state, ] ## subset table for requested state  
   
-  ##The num argument can take values "best", "worst", or an integer indicating the ranking
-  ##(smaller numbers are better). If the number given by num is larger than the number of hospitals in that
-  ##state, then the function should return NA. 
+  ## read outcome values
+  colnum <- outcomes$c[outcome == outcomes$n] ## find column name for requested outcome
+  stateoutcomes[, colnum] <- as.numeric(stateoutcomes[, colnum]) ## subset vector of requested outcome rate values
+  numstatehosp <- sum(!is.na(stateoutcomes[, colnum]))
   
-  ##Hospitals that do not have data on a particular outcome should
-  ##be excluded from the set of hospitals when deciding the rankings.
+  ##The num argument can take values "best", "worst", or an integer indicating the ranking (smaller numbers are better).
+  ##If the number given by num is larger than the number of hospitals in that state, then the function should return NA.
+  ##Hospitals that do not have data on a particular outcome should be excluded from the set of hospitals when deciding the rankings.
   
-  ## If multiple hospitals have the same 30-day mortality rate for a given cause
-  ##of death, ties should be broken by using the hospital name.
-  ##One can use the order function to sort multiple vectors in this
-  ##manner (i.e. where one vector is used to break ties in another vector).
+    if(num == "best"){
+    num <- 1
+  } else if(num == "worst"){
+    num <- numstatehosp
+  } else if(class(num) != "numeric"){
+    stop("invalid num")
+  } else{
+    num <- round(num, 0)
+    if(num > numstatehosp){
+      return(NA)
+    }
+  }
+  
+  ##If multiple hospitals have the same 30-day mortality rate for a given cause of death, ties should be broken by using the hospital name.
+  ##One can use the order function to sort multiple vectors in this manner (i.e. where one vector is used to break ties in another vector).
+  orderedstateoutcomes <- stateoutcomes[order(stateoutcomes[, colnum], stateoutcomes[, "Hospital.Name"]),c(2,colnum)]
   
   ## Return hospital name in that state with the given rank 30-day death rate
+  orderedstateoutcomes[num,"Hospital.Name"]
 }
