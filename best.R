55996ff4e95df1447929219bd964604ce55ce81a
Anna Pestereva <antrinna@gmail.com>
2018-08-15 15:01:03 -0600

    complete best function, find the best hospital in a state for a particular outcome

--- best.R
+++ best.R
@@ -1,20 +1,34 @@
 best <- function(state, outcome) {
+  
   ## Read outcome data
   outcomedata <- read.csv("outcome-of-care-measures.csv", colClasses = "character")
-  
+
   ## Check that state and outcome are valid
-  ## If an invalid state value is passed to best, the  function should throw an error via the stop function with the exact message "invalid state". If an invalid
-  ## outcome value is passed to best, the function should throw an error via the stop function with the exact message "invalid outcome".
+  states <- levels(as.factor(outcomedata$State)) ## what if csv doesn't contain all states?
+  outcomes <- list(n = c("heart attack", "heart failure", "pneumonia"), c = c(11, 17, 23)) # also save column nums
   
-  ## The hospital name is the name provided in the Hospital.Name variable. The outcomes can
-  ## be one of "heart attack", "heart failure", or "pneumonia".
+  ## If an invalid state value is passed to best, throw an error via the stop function with the exact message "invalid state". 
+  if(!(state %in% states)){
+    stop("invalid state")
+  }
   
-  ## sample reading outcome values
-  ## outcomedata[, 11] <- as.numeric(outcomedata[, 11])
+  ## If an invalid outcome value is passed to best, the function should throw an error via the stop function with the exact message "invalid outcome".
+  if(!(outcome %in% outcomes$n)){
+    stop("invalid outcome")
+  }
   
+  ## read outcome values
+  stateoutcomes <- outcomedata[outcomedata$State == state, ] ## subset table for requested state
+  colnum <- outcomes$c[outcome == outcomes$n] ## find column name for requested outcome
+  stateoutcomerates <- as.numeric(stateoutcomes[, colnum]) ## subset vector of requested outcome rate values
+ 
   ## Return hospital name in that state with lowest 30-day death
   ## Hospitals that do not have data on a particular outcome should be excluded from the set of hospitals when deciding the rankings.
-  ##  If there is a tie for the best hospital for a given outcome, then the hospital names should
+  ##If there is a tie for the best hospital for a given outcome, then the hospital names should
   ## be sorted in alphabetical order and the first hospital in that set should be chosen
-  ## rate
+  bestoutcomerate <- min(stateoutcomerates, na.rm = TRUE) ## find minimum rate for the requested subset
+  bestrecords <- stateoutcomerates == bestoutcomerate & !is.na(stateoutcomerates) ## records with minimum rate
+  besthospitals <- stateoutcomes[bestrecords,"Hospital.Name"] ## look up hospital names for the best records
+  
+  min(besthospitals) ## return first hospital name alphabetically
 }
