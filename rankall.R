a8980a5134185ff6b7960b2f1d7201f0630b834a
Anna Pestereva <antrinna@gmail.com>
2018-08-15 17:52:34 -0600

    rankall function template

---
+++ rankall.R
@@ -0,0 +1,43 @@
+rankall <- function(outcome, num = "best") {
+  
+  ## Read outcome data
+  outcomedata <- read.csv("outcome-of-care-measures.csv", colClasses = "character")
+  
+  ## Check that outcome and num are valid
+  states <- levels(as.factor(outcomedata$State)) ## what if csv doesn't contain all states?
+  outcomes <- list(n = c("heart attack", "heart failure", "pneumonia"), c = c(11, 17, 23)) # also save column nums
+
+  ## If an invalid outcome value is passed to best, the function should throw an error via the stop function with the exact message "invalid outcome".
+  if(!(outcome %in% outcomes$n)){
+    stop("invalid outcome")
+  }
+
+  # stateoutcomes <- outcomedata[outcomedata$State == state, ] ## subset table for requested state  
+  
+  ## read outcome values
+  colnum <- outcomes$c[outcome == outcomes$n] ## find column name for requested outcome
+  # stateoutcomes[, colnum] <- as.numeric(stateoutcomes[, colnum]) ## subset vector of requested outcome rate values
+  # numstatehosp <- sum(!is.na(stateoutcomes[, colnum]))
+  
+  ##The num argument can take values "best", "worst", or an integer indicating the ranking (smaller numbers are better).
+  ##If the number given by num is larger than the number of hospitals in that state, then the function should return NA.
+  ##Hospitals that do not have data on a particular outcome should be excluded from the set of hospitals when deciding the rankings.
+  
+    if(num == "best"){
+    num <- 1
+  } else if(num == "worst"){
+    # num <- numstatehosp
+  } else if(class(num) != "numeric"){
+    stop("invalid num")
+  } else{
+    num <- round(num, 0)
+    # if(num > numstatehosp){
+    #   return(NA)
+    # }
+  }
+  
+  ## For each state, find the hospital of the given rank
+  
+  ## Return a data frame with the hospital names and the (abbreviated) state name
+  
+}
